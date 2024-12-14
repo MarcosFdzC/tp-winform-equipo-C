965975fdf674ede526aa5f6be4e9865c2963ee4e
Marcos Fernandez <marcosfernandezc1998@gmail.com>
2024-12-14 13:03:09 -0300

    solución de conflicto de git

--- TPWinForm_equipo-C/dominio/Articulo.cs
+++ TPWinForm_equipo-C/dominio/Articulo.cs
@@ -8,9 +8,6 @@ namespace dominio
 {
     public class Articulo
     {
-<<<<<<< HEAD
-
-=======
         public string codigo {  get; set; }
         public string nombre { get; set; }
         public string descripcion { get; set; }
@@ -18,6 +15,5 @@ namespace dominio
         public Categoria categoria { get; set; } // asociar con la clase Categoria
         public Imagen imagenes { get; set; } // asociar con la clase Imagen
         public int precio { get; set; }
->>>>>>> f97487a48ff67f2f91b8507ac16cd788703b6d9a
     }
 }
