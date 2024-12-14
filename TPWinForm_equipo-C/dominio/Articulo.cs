f97487a48ff67f2f91b8507ac16cd788703b6d9a
Rafa2626 <rafaeldeacon15@gmail.com>
2024-12-13 21:16:56 -0300

    Atributos /a medias/ de clase Articulo

--- TPWinForm_equipo-C/dominio/Articulo.cs
+++ TPWinForm_equipo-C/dominio/Articulo.cs
@@ -8,5 +8,12 @@ namespace dominio
 {
     public class Articulo
     {
+        public string codigo {  get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public Marca marca { get; set; } // asociar con la clase Marca
+        public Categoria categoria { get; set; } // asociar con la clase Categoria
+        public Imagen imagenes { get; set; } // asociar con la clase Imagen
+        public int precio { get; set; }
     }
 }
